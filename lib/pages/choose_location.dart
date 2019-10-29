7cfb4d0aca9ed63bfa6e23ba9e4f49ce57302604
kyeongmin <hotkoco@naver.com>
2019-10-29 22:35:31 +0900

    add world time api

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,31 +6,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
-  void getData() async {
-    //simulate network request for a username
-    String username = await Future.delayed(Duration(seconds: 3), () {
-      print('a');
-      return 'yoshi';
-    });
-
-    //simulate network request to get bio of the username
-    String bio = await Future.delayed(Duration(seconds: 2), () {
-      return 'vega, musician & egg collector';
-    });
-
-    print('$username - $bio');
-  }
-
-  int counter = 0;
-
-  @override  //상속받은 클래스의 것을 오버라이드
-  void initState() {
-    super.initState();
-    getData();
-    print('hey there!');
-  }
-
   @override
   Widget build(BuildContext context) {
     print('build function ran');
@@ -41,14 +16,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0,
-      ),
-      body: RaisedButton(
-        onPressed: (){
-          setState(() {
-            counter++;
-          });
-        },
-        child: Text('counter is $counter'),
       ),
     );
   }
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/Services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +7,35 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override  //상속받은 클래스의 것을 오버라이드
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      appBar: AppBar(
+        title: Text('loading'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(50),
+        child: Text(time),
+      ),
     );
   }
 }
